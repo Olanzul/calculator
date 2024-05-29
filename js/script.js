f8ce34182d78a8b8e074af8a898d2a4ec2589ac5
Olanzul <olanzul@gmail.com>
2024-05-30 00:23:14 +0700

    fixing css

--- js/script.js
+++ js/script.js
@@ -4,58 +4,57 @@ let previousOperand = '';
 let operation = undefined;
 
 function appendNumber(number) {
-    if (number === 0 && currentOperand === '') return;
-    currentOperand = currentOperand.toString() + number.toString();
-    updateDisplay();
+  if (number === 0 && currentOperand === "") return;
+  currentOperand = currentOperand.toString() + number.toString();
+  updateDisplay();
 }
 
 function chooseOperation(op) {
-    if (currentOperand === '') return;
-    if (previousOperand !== '') {
-        compute();
-    }
-    operation = op;
-    previousOperand = currentOperand;
-    currentOperand = '';
+  if (currentOperand === "") return;
+  if (previousOperand !== "") {
+    compute();
+  }
+  operation = op;
+  previousOperand = currentOperand;
+  currentOperand = "";
 }
 
 function compute() {
-    let result;
-    const prev = parseFloat(previousOperand);
-    const current = parseFloat(currentOperand);
-    if (isNaN(prev) || isNaN(current)) return;
+  let result;
+  const prev = parseFloat(previousOperand);
+  const current = parseFloat(currentOperand);
+  if (isNaN(prev) || isNaN(current)) return;
 
-    switch (operation) {
-        case '+':
-            result = prev + current;
-            break;
-        case '-':
-            result = prev - current;
-            break;
-        case '*':
-            result = prev * current;
-            break;
-        case '/':
-            result = prev / current;
-            break;
-        default:
-            return;
-    }
+  switch (operation) {
+    case "+":
+      result = prev + current;
+      break;
+    case "-":
+      result = prev - current;
+      break;
+    case "*":
+      result = prev * current;
+      break;
+    case "/":
+      result = prev / current;
+      break;
+    default:
+      return;
+  }
 
-    currentOperand = result;
-    operation = undefined;
-    previousOperand = '';
-    updateDisplay();
+  currentOperand = result;
+  operation = undefined;
+  previousOperand = "";
+  updateDisplay();
 }
 
 function clearDisplay() {
-    currentOperand = '';
-    previousOperand = '';
-    operation = undefined;
-    updateDisplay();
+  currentOperand = "";
+  previousOperand = "";
+  operation = undefined;
+  updateDisplay();
 }
 
 function updateDisplay() {
-    display.innerText = currentOperand;
+  display.innerText = currentOperand;
 }
-
